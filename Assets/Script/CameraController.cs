98b8da40a095e96d1c47d5a5c3606494c0878c9c
mikushimizu <shimizu.miku43239@gmail.com>
2018-04-28 12:52:27 +0900

    TestHelloWorld

---
+++ Assets/Script/CameraController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+	public GameObject target;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = target.transform.position + new Vector3 (0, 5.7f, -2.9f);
+		//transform.rotation = target.transform.rotation + new Vector3 (62, 0, 0);
+
+	}
+}
---
+++ Assets/Script/PlayerController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour {
+
+	float speed = 5.0f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if ( this.transform.position.y <= -10) {
+			this.transform.position = new Vector3 (0, 18.5f, 0);
+			Debug.Log ("Fall");
+		}
+
+		if (Input.GetKey ("up")) {
+			transform.position += new Vector3 (0, 0, speed * Time.deltaTime);
+			Debug.Log ("Go");
+		}
+		if (Input.GetKey ("down")) {
+			transform.position += new Vector3 (0, 0, -speed * Time.deltaTime);
+			Debug.Log ("Back");
+		}
+		if (Input.GetKey ("left")) {
+			transform.position += new Vector3 (-speed * Time.deltaTime,0,0);
+			Debug.Log ("left");
+		}
+		if (Input.GetKey ("right")) {
+			transform.position += new Vector3 (speed * Time.deltaTime,0,0);
+			Debug.Log ("right");
+		}
+		
+	}
+
+
+
+
+
+}
