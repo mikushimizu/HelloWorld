9484d4c668ba74f01f5e3d52098781472f157708
mikushimizu <shimizu.miku43239@gmail.com>
2018-05-04 22:52:07 +0900

    【α版提出】 ・移動 ・ついたり消えたりするレーザー(赤) ・制限時間表示 ・ボタンに触れると解除されるレーザー(緑) ・ビジュアル面少し ・文章表示UI少し
    α版でセーブ地点(青いオーラ)に触れると、位置と時間をセーブする機能をつけたかったのですが、うまくいきませんでした…。
    アイテムの所持も記録したいので、セーブ機能の実装は思っていたよりかなり大変そうです…。後程GitHubのIssueで質問します。
    最悪セーブ機能なしの鬼畜ゲーにもできるので、今後はまず、ステージ・アイテム2種類・テキストイベントを作成したいと思います。

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -7,34 +7,32 @@ using UnityEngine.SceneManagement;
 public class PlayerController : MonoBehaviour {
 
 	float speed = 5.0f;
+	public Vector3 s_position;
+	public GameObject Switch1;
+	public GameObject LaserGreen;
 
 	// Use this for initialization
 	void Start () {
-		
+		s_position = new Vector3(transform.position.x, transform.position.y, transform.position.z );
 	}
 
 	// Update is called once per frame
 	void Update () {
 		if ( this.transform.position.y <= -10) {
 			this.transform.position = new Vector3 (0, 18.5f, 0);
-			Debug.Log ("Fall");
 		}
 
 		if (Input.GetKey ("up")) {
 			transform.position += new Vector3 (0, 0, speed * Time.deltaTime);
-			Debug.Log ("Go");
 		}
 		if (Input.GetKey ("down")) {
 			transform.position += new Vector3 (0, 0, -speed * Time.deltaTime);
-			Debug.Log ("Back");
 		}
 		if (Input.GetKey ("left")) {
 			transform.position += new Vector3 (-speed * Time.deltaTime,0,0);
-			Debug.Log ("left");
 		}
 		if (Input.GetKey ("right")) {
 			transform.position += new Vector3 (speed * Time.deltaTime,0,0);
-			Debug.Log ("right");
 		}
 		
 	}
@@ -42,8 +40,14 @@ public class PlayerController : MonoBehaviour {
 	void OnCollisionEnter(Collision other){
 		if (other.gameObject.tag == "Laser") {
 			SceneManager.LoadScene ("Gameover");
+			this.transform.position = new Vector3 (s_position.x, s_position.y, s_position.z);
+		}
+		if (other.gameObject.tag == "Switch1") {
+			Switch1.gameObject.SetActive(false);
+			LaserGreen.gameObject.SetActive(false);
 		}
 	}
+
 
 
 
---
+++ Assets/Script/SaveScript.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SaveScript : MonoBehaviour {
+
+	void OnTriggerEnter2D(Collider2D other){
+		if (other.tag == "Player"){
+			//衝突した場所を取得
+			GameObject.Find("Player").GetComponent<PlayerController>().s_position.x = transform.position.x;
+			GameObject.Find("Player").GetComponent<PlayerController>().s_position.z = transform.position.z;
+			Debug.Log ("xは" + transform.position.x + ", zは" + transform.position.z);
+		}
+	}
+}
